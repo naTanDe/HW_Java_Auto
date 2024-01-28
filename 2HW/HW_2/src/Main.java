5c16b9ca69f1d97efeff4649e3e7b846410ed4c4
naTanDe <124785715+naTanDe@users.noreply.github.com>
2024-01-28 18:48:08 +0700

    Обновление HW2_1

--- 2HW/HW_2/src/Main.java
+++ 2HW/HW_2/src/Main.java
@@ -4,11 +4,16 @@ public class Main {
     public static void main(String[] args) {
         int a = 23;
         int b = 5;
-        System.out.println(a + b);
-        System.out.println(a * b);
-        System.out.println(a - b);
-        System.out.println(a / b);
-        System.out.println(a % b);
+        int s = a + b;
+        int u = a * b;
+        int r = a - b;
+        int d = a / b;
+        int n = a % b;
+        System.out.println(s);
+        System.out.println(u);
+        System.out.println(r);
+        System.out.println(d);
+        System.out.println(n);
 
     }
 }
