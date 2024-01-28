a56603372699a885f694c552e04012bf24fb9666
naTanDe <124785715+naTanDe@users.noreply.github.com>
2024-01-28 17:57:01 +0700

    Обновление HW1_1

---
+++ 1HW/First_Project/src/First.java
@@ -0,0 +1,10 @@
+import java.sql.SQLOutput;
+
+public class First {
+    public static void main(String[] args) {
+        for (int i = 3; i >= 1; i--) {
+            System.out.println(i);
+        }
+        System.out.println("Поехали!");
+    }
+}
---
+++ 1HW/untitled/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 3; i = 0; i --) {
+            Println
+        }
+
+    }
+}
