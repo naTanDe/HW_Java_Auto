915c2caa519601ff4d165f4468a6639422323e6e
naTanDe <124785715+naTanDe@users.noreply.github.com>
2024-02-03 11:13:38 +0700

    update hw3

--- HW3/HW_3/src/HW3.java
+++ HW3/HW_3/src/HW3.java
@@ -1,9 +1,29 @@
+import java.util.Scanner;
 public class HW3 {
     public static void main(String[] args) {
         int a;
         int b;
+        System.out.println("Введите число а = ");
+        Scanner console_a = new Scanner(System.in);
+        a = console_a.nextInt();
 
+        System.out.println("Введите число b = ");
+        Scanner console_b = new Scanner(System.in);
+        b = console_b.nextInt();
 
-
+        if (a == b) {
+            System.out.println("a == b");
+        } else {
+        if (a < b) {
+            System.out.println("a < b");
+        }  else {
+            System.out.println("a > b");
+            }
+        } if (((a + b) % 2) == 0) {
+            System.out.println("а и б скорее всего четные");
+        } else {
+            System.out.println("some variable is odd");
+        }
     }
 }
+
