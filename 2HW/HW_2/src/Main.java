83ccd75db98aa5bac35dffd3d2385bc595b0aeed
naTanDe <124785715+naTanDe@users.noreply.github.com>
2024-02-03 09:53:12 +0700

    update hw3

--- 2HW/HW_2/src/Main.java
+++ 2HW/HW_2/src/Main.java
@@ -2,12 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int a = 23;
+        int a = 31;
         int b = 5;
+        System.out.println("Деление цеорчисленное " + a / b);
         int s = a + b;
         int u = a * b;
         int r = a - b;
-        int d = a / b;
+        double d = 1.0 * a / b;
         int n = a % b;
         System.out.println(s);
         System.out.println(u);
@@ -15,10 +16,10 @@ public class Main {
         System.out.println(d);
         System.out.println(n);
         if (a % 2 == 0) {
-            System.out.println("a = " + a + "четное");
+            System.out.println("a = " + a + " четное");
         }
         if (b % 2 == 0) {
-            System.out.println("b = " + b + "четное");
+            System.out.println("b = " + b + " четное");
         } else {
             System.out.println("a и b нечетные");
         }
---
+++ HW3/HW_3/src/HW3.java
@@ -0,0 +1,9 @@
+public class HW3 {
+    public static void main(String[] args) {
+        int a;
+        int b;
+
+
+
+    }
+}
