2194b95128376aad7eebb3e5c6e501f728d885a4
naTanDe <124785715+naTanDe@users.noreply.github.com>
2024-02-03 12:49:14 +0700

    finish hw2

--- HW3/HW_3/src/HW3.java
+++ HW3/HW_3/src/HW3.java
@@ -15,15 +15,37 @@ public class HW3 {
             System.out.println("a == b");
         } else {
         if (a < b) {
-            System.out.println("a < b");
+            System.out.println("1. a < b");
         }  else {
-            System.out.println("a > b");
+            System.out.println("1. a > b");
             }
         } if (((a + b) % 2) == 0) {
-            System.out.println("а и б скорее всего четные");
+            System.out.println("2. а и б скорее всего четные");
         } else {
-            System.out.println("some variable is odd");
+            System.out.println("2. какая то пересменная не четная");
         }
+
+        if (a > 10 || b > 10 ) {
+            System.out.println("3. a или b больше 10");
+        } if (a < 100 || b < 100) {
+            System.out.println("4. a или b меньше 100");
+        } if ((a / 2) > 20 ) {
+            System.out.println("5. Результат деления а на 2 больше 20");
+        } if ((b / 2) > 20 ) {
+            System.out.println("5. Результат деления b на 2 больше 20");
+
+        } if (a >= 5 & a <= 40) {
+            System.out.println("6. значение переменной a между 5 и 40 включительно");
+        } else {
+            System.out.println("6. значение переменной a меньше 5 или больше 40");
+        }
+        if (b >= 5 & b <= 40) {
+            System.out.println("7. значение переменной b между 5 и 40 включительно");
+        } else {
+            System.out.println("7. значение переменной b меньше 5 или больше 40");
+        }
+
+
     }
 }
 
