a057c13202e0882cf4b47828d10492eaa348256d
naTanDe <124785715+naTanDe@users.noreply.github.com>
2024-02-10 20:18:28 +0700

    update hw5_2

--- HW5/src/HW5_2.java
+++ HW5/src/HW5_2.java
@@ -1,5 +1,41 @@
 public class HW5_2 {
     public static void main(String[] args) {
-        
-    }
-}
+
+        System.out.println("Задача №1");
+//        Вывести следующие строки с соответствующим форматированием (как пирамиды):
+////
+//        Задача №1
+//
+//        0  1  2  3  4  5  6  7  8  9
+//        0  1  2  3  4  5  6  7  8
+//        0  1  2  3  4  5  6  7
+//        0  1  2  3  4  5  6
+//        0  1  2  3  4  5
+//        0  1  2  3  4
+//        0  1  2  3
+//        0  1  2
+//        0  1
+//        0
+
+        for (int i = 9; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Задача №2");
+
+        for (int i = 0; i <  )
+
+                // Обраный счет
+
+        /*for (int i = 0; i <= 9; i++) {
+            for (int j = 9; j >= i; j--) {
+                System.out.print(j - i  + " ");
+            }
+            System.out.println();
+        }*/
+
+            }
+        }
